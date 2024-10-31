8beace5f69d15fbebb940fbb02dae4363a7ff78c
Timo Behrendt <t.behrendt@t00n.de>
2024-10-31 18:34:59 +0100

    cleanup validateEventId

--- src/routes/api/event/[eventid]/+server.ts
+++ src/routes/api/event/[eventid]/+server.ts
@@ -17,7 +17,7 @@ export async function GET(e) {
 }
 
 // Check if the event ID is a UUID
-function validateEventId(eventId: string): boolean {
+export function validateEventId(eventId: string): boolean {
 	return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
 		eventId
 	);
